0cb287e84e34fc7fb5ae05aa2445b50fe0969aed
sealion <sealion@charadeyouare.org>
2012-01-04 16:34:55 +0400

    Improved cal, weeks start on Monday (default) or Sunday (-S).

--- cal.hs
+++ cal.hs
@@ -3,60 +3,41 @@ import Data.Time.Calendar (gregorianMonthLength)
 import Data.List (intercalate)
 import System.Environment (getArgs)
 
-days :: [(Day, Int)]
-days = [(Monday, 1),
-        (Tuesday, 2),
-        (Wednesday, 3),
-        (Thursday, 4),
-        (Friday, 5),
-        (Saturday, 6),
-        (Sunday, 7)]
+dayToInt :: Day -> Bool -> Int
+dayToInt day mondayFirst =
+    if mondayFirst then
+        if i == 0 then 6 else i - 1
+    else i
+    where i = fromEnum day
 
-months :: [(Month, Int)]
-months = [(January, 1),
-          (February, 2),
-          (March, 3),
-          (April, 4),
-          (May, 5),
-          (June, 6),
-          (July, 7),
-          (August, 8),
-          (September, 9),
-          (October, 10),
-          (November, 11),
-          (December, 12)]
+intToDay :: Int -> Bool -> Day
+intToDay x mondayFirst =
+    let i = if mondayFirst then
+                if x == 6 then 0 else x + 1
+            else x
+    in toEnum i :: Day
 
-toRange :: Int -> Int
-toRange d
-    | d < 1 = toRange $ d + 7
-    | d > 7 = toRange $ d - 7
-    | otherwise = d
-
-getCal :: CalendarTime -> [[Maybe Int]]
-getCal time =
-    let wday = case lookup (ctWDay time) days of (Just x) -> x
-        fwday = toRange $ wday - (ctDay time - 1)
+getCal :: CalendarTime -> Bool -> [[Maybe Int]]
+getCal time mondayFirst =
+    let wday = dayToInt (ctWDay time) mondayFirst
+        fwday' =  wday - ((ctDay time - 1) `mod` 7)
+        fwday = if fwday' < 0 then fwday' + 7 else fwday'
         year = toInteger $ ctYear time
-        month = case lookup (ctMonth time) months of (Just x) -> x
+        month = (fromEnum $ ctMonth time) + 1
         mlen = gregorianMonthLength year month
-    in build fwday mlen
+        initialWeek = replicate fwday Nothing
+    in build [] initialWeek 1 mlen
 
-build :: Int -> Int -> [[Maybe Int]]
-build fwday mlen =
-    let month = []
-        week = replicate (fwday - 1) Nothing
-    in build' month week 1 mlen
-
-build' :: [[Maybe Int]] -> [Maybe Int] -> Int -> Int -> [[Maybe Int]]
-build' month week day maxday =
+build :: [[Maybe Int]] -> [Maybe Int] -> Int -> Int -> [[Maybe Int]]
+build month week day maxday =
     if length week + 1 == 7 then
         if day == maxday then
             month ++ [week']
-        else build' (month ++ [week']) [] (day + 1) maxday
+        else build (month ++ [week']) [] (day + 1) maxday
     else
         if day == maxday then
             month ++ [week' ++ (replicate (6 - length week) Nothing)]
-        else build' month week' (day + 1) maxday
+        else build month week' (day + 1) maxday
     where week' = week ++ [Just day]
 
 showDay :: (Maybe Int) -> String
@@ -65,11 +46,10 @@ showDay (Just day) = if length s == 1 then ' ':s else s
 showDay Nothing = "  "
 
 showWeek :: [Maybe Int] -> String
-showWeek week = intercalate " " (map showDay week)
+showWeek = intercalate " " . map showDay
 
-getCalendarTime :: IO CalendarTime
-getCalendarTime = do
-    args <- getArgs
+getCalendarTime :: [String] -> IO CalendarTime
+getCalendarTime args = do
     if (length args) == 2 then
         getCalendarTimeFromArgs args
         else do
@@ -80,17 +60,20 @@ getCalendarTime = do
 getCalendarTimeFromArgs :: [String] -> IO CalendarTime
 getCalendarTimeFromArgs args = do
     let year = read $ args !! 0
-        month = getMonth $ read $ args !! 1
+        month = toEnum ((read $ args !! 1) - 1)
         ct = CalendarTime year month 1 0 0 0 0 Sunday 0 "UTC" 0 False
     toCalendarTime $ toClockTime ct
-    where getMonth n = case lookup n (map swap months) of (Just x) -> x
-          swap (a,b) = (b,a)
 
 main = do
-    time <- getCalendarTime
+    args <- getArgs
+    let firstDayFlag = (length args) > 0 && ((args !! 0) `elem` ["-M", "-S"])
+        mondayFirst = if firstDayFlag then args !! 0 == "-M" else True
+        args' = if firstDayFlag then drop 1 args else args
+    time <- getCalendarTime args'
     let header = (show $ ctMonth time) ++ " " ++ (show $ ctYear time)
-        days = "Mo Tu We Th Fr Sa Su"
-        dif = fromIntegral $ (length days) - (length header)
+        labels = intercalate " " $ map (\x -> take 2 $ show $ intToDay x mondayFirst) [0..6]
+        dif = fromIntegral $ (length labels) - (length header)
         header' = if dif > 0 then (replicate (floor (dif / 2)) ' ') ++ header else header
-    mapM putStrLn (header':days:(map showWeek (getCal time)))
+    mapM putStrLn (header':labels:(map showWeek $ getCal time mondayFirst))
+    return ()
 
